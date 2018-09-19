724b83521e955842da63d16b4d16b587a46a521c
Sarah Waldie <sarah@sassafrasbiz.com>
2018-09-19 13:41:45 -0400

    Udpated JS so topnav bar hides on scroll

--- js/app.js
+++ js/app.js
@@ -1,13 +1,16 @@
 function navShrink() {
 
-    const header = document.querySelector('header')
+    const header = document.querySelector('header');
+    const topnav = document.querySelector('.top-nav-bar');
     document.addEventListener("scroll", function () {
         if
         (window.pageYOffset > 70) {
             header.classList.add("white");
+            topnav.style.top = "-40px";
         }
         else {
             header.classList.remove("white");
+            topnav.style.top = "0";
         }
     });
 };
